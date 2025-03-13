37ae05eb89f352a40afc32a606e5a8383ea0ff87
Daniel <Danielrsanchez1990@gmail.com>
2025-03-13 01:26:32 -0700

    Major stuff added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Home.css"
+
+function Home() {
+    return (
+        <div>
+            <h1 className="Big-Name"> Daniel<br />Sanchez<br /> </h1>
+
+            <div className="small-about">
+                <p>A passionate frontend developer</p>
+            </div>
+
+            <div>
+                
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
