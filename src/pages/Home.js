37b45b61c9044bd3ecf82d0da3eba7ddfba01cb0
Daniel <Danielrsanchez1990@gmail.com>
2025-03-18 04:51:58 -0700

    Reconstructed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,6 @@ import "../styles/Home.css"
 function Home() {
     return (
         <div>
-            <h1 className="Big-Name"> Daniel<br />Sanchez<br /> </h1>
-
-            <div className="small-about">
-                <p>A passionate frontend developer</p>
-            </div>
-
-            <div className="Moon-Container">
-                <div className="eclipse-1"></div>
-            </div>
 
         </div>
     );
