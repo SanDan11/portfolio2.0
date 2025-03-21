7ff7319830edc4e0a72143e6e7839bb583497d45
SanDan11 <Danielrsanchez1990@gmail.com>
2025-03-21 16:38:53 -0700

    Pages and style added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,19 @@ import "../styles/Home.css"
 function Home() {
     return (
         <div>
+            <h1 className="name-logo">Daniel Sanchez</h1>
+
+            <div className="left-container">
+                <p>I am Left</p>
+            </div>
+
+            <div className="center-container">
+                <p>I am center</p>
+            </div>
+            
+            <div className="right-container">
+                <p>I am right</p>
+            </div>
 
         </div>
     );
